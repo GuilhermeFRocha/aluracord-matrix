6d94f8fb92ea548db88824f6ccc79681558e3528
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-01-26 12:27:17 -0300

    adicionando chat

--- pages/index.js
+++ pages/index.js
@@ -4,35 +4,6 @@ import React from 'react';
 import {useRouter} from 'next/router'
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
-
 function Titulo (props) {
   const Tag = props.tag || 'h1';
   return (
@@ -66,11 +37,11 @@ function Titulo (props) {
 export default function PaginaInicial() {
   // const username = 'GuilhermeFRocha';
   const [username, setUsername] = React.useState('peas');
+  const roteamento = useRouter();
 
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -99,7 +70,7 @@ export default function PaginaInicial() {
             as="form"
             onSubmit = {function (event) {
               event.preventDefault();
-              window.location.href = '/chat';
+              roteamento.push('/chat')
             }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
